ae68b7799e2228fbcda7851c02f62cd5ae9c5237
tpoppi <61734281+tpoppi@users.noreply.github.com>
2020-04-18 11:39:58 +0200

    Add files via upload

--- ProgettoScacchi/mosseax.cpp
+++ ProgettoScacchi/mosseax.cpp
@@ -10,10 +10,10 @@ std::vector<int> Mosseax::move() const
         int i=pos+7;
         int limitesuperiore=8-pos/8;
         int j=1;
-        while(i>=0 && (pos%8)!=0 && parent->getStato(i)!=bianco && j<limitesuperiore){//mosse in diagonale in alto a sinistra
+        while(i>=0 && i<64 && (pos%8)!=0 && parent->getStato(i)!=bianco && j<limitesuperiore){//mosse in diagonale in alto a sinistra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==nero || (i%8)==0)
-                j=limitesuperiore;
+                break;
             else{
                 i+=7;
                 j++;
@@ -21,10 +21,10 @@ std::vector<int> Mosseax::move() const
         }
         i=pos+9;
         j=1;
-        while(i>=0 && (pos%8)!=7 && parent->getStato(i)!=bianco && j<limitesuperiore){//mosse in diagonale in alto a destra
+        while(i>=0 && i<64 && (pos%8)!=7 && parent->getStato(i)!=bianco && j<limitesuperiore){//mosse in diagonale in alto a destra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==nero || (i%8)==7)
-                j=limitesuperiore;
+                break;
             else{
                 i+=9;
                 j++;
@@ -33,10 +33,10 @@ std::vector<int> Mosseax::move() const
         i=pos-9;
         j=1;
         int limiteinferiore=pos/8;
-        while(i>=0 && (pos%8)!=0 && parent->getStato(i)!=bianco && j<=limiteinferiore){//mosse in diagonale in basso a sinistra
+        while(i>=0 && i<64 && (pos%8)!=0 && parent->getStato(i)!=bianco && j<=limiteinferiore){//mosse in diagonale in basso a sinistra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==nero || (i%8)==0)
-                j=limiteinferiore+1;
+                break;
             else{
                 i-=9;
                 j++;
@@ -44,10 +44,10 @@ std::vector<int> Mosseax::move() const
         }
         i=pos-7;
         j=1;
-        while(i>=0 && (pos%8)!=7 &&  parent->getStato(i)!=bianco && j<=limiteinferiore){//mosse in diagonale in basso a destra
+        while(i>=0 && i<64 && (pos%8)!=7 &&  parent->getStato(i)!=bianco && j<=limiteinferiore){//mosse in diagonale in basso a destra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==nero || (i%8)==7)
-                j=limiteinferiore+1;
+                break;
             else{
                 i-=7;
                 j++;
@@ -58,10 +58,10 @@ std::vector<int> Mosseax::move() const
         int i=pos+7;
         int limitesuperiore=8-pos/8;
         int j=1;
-        while( i>=0 && (pos%8)!=0 && parent->getStato(i)!=nero && j<limitesuperiore){//mosse in diagonale in alto a sinistra
+        while( i>=0 && i<64 && (pos%8)!=0 && parent->getStato(i)!=nero && j<limitesuperiore){//mosse in diagonale in alto a sinistra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==bianco || (i%8)==0)
-                j=limitesuperiore;
+                break;
             else{
                 i+=7;
                 j++;
@@ -69,10 +69,10 @@ std::vector<int> Mosseax::move() const
         }
         i=pos+9;
         j=1;
-        while(i>=0 && (pos%8)!=7 &&  parent->getStato(i)!=nero && j<limitesuperiore){//mosse in diagonale in alto a destra
+        while(i>=0 && i<64 && (pos%8)!=7 &&  parent->getStato(i)!=nero && j<limitesuperiore){//mosse in diagonale in alto a destra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==bianco || (i%8)==7)
-                j=limitesuperiore;
+                break;
             else{
                 i+=9;
                 j++;
@@ -81,10 +81,10 @@ std::vector<int> Mosseax::move() const
         i=pos-9;
         j=1;
         int limiteinferiore=pos/8;
-        while(i>=0 && (pos%8)!=0 && parent->getStato(i)!=nero && j<=limiteinferiore){//mosse in diagonale in basso a sinistra
+        while(i>=0 && i<64 && (pos%8)!=0 && parent->getStato(i)!=nero && j<=limiteinferiore){//mosse in diagonale in basso a sinistra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==bianco || (i%8)==0)
-                j=limiteinferiore+1;
+                break;
             else{
                 i-=9;
                 j++;
@@ -92,10 +92,10 @@ std::vector<int> Mosseax::move() const
         }
         i=pos-7;
         j=1;
-        while(i>=0 && (pos%8)!=7 &&  parent->getStato(i)!=nero && j<=limiteinferiore){//mosse in diagonale in basso a destra
+        while(i>=0 && i<64 && (pos%8)!=7 &&  parent->getStato(i)!=nero && j<=limiteinferiore){//mosse in diagonale in basso a destra
             mossepossibili.push_back(i);
             if(parent->getStato(i)==bianco || (i%8)==7)
-                j=limiteinferiore+1;
+                break;
             else{
                 i-=7;
                 j++;
