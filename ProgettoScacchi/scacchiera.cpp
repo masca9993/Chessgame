4de759b0df285e59507ad50155f83df77959b3dc
tpoppi <61734281+tpoppi@users.noreply.github.com>
2020-04-23 13:05:46 +0200

    Update scacchiera.cpp

--- ProgettoScacchi/scacchiera.cpp
+++ ProgettoScacchi/scacchiera.cpp
@@ -420,7 +420,7 @@ giocatore Scacchiera::Winner(bool p)   //ipotizzo che il re sia sottoscacco, per
 
 }
 
-
+/*
 void Scacchiera::doMove(int pos1, int pos2)
 {
     if (board[pos1])
@@ -494,5 +494,73 @@ void Scacchiera::doMove(int pos1, int pos2)
   }
           throw Mossa_Imposs();
 }
+*/
+void Scacchiera::doMove(int pos1, int pos2)
+{
+          if (board[pos1])
+          {
+            std::vector<int> v=board[pos1]->move();
+            for(unsigned int i=0; i<v.size();i++){
+                if(v[i]==pos2){
+                    if (i>=v.size()-2 && typeid(*getPedina(pos1))==typeid(Re)) //se devo muovere il re e la mossa è nelle ultime due posizioni devo fare l'arrocco
+                    {
+                     Arrocco(pos1, v[i]);
+                        return;
+                    }
+                    else
+                    {
+                        if(typeid(*getPedina(pos1))==typeid(Pedone)){
+                            std::vector<int> p=dynamic_cast<Pedone*>(getPedina(pos1))->enpassant();
+                            for(unsigned int y=0;y<p.size();y++){
+                                if(pos2==p[y]){
+                                    Scacchiera* prova=new Scacchiera(*this);
+                                    if(prova->board[pos1]->getColore()){
+                                        //delete prova->board[pos2-8];
+                                        prova->board[pos2-8]=nullptr;
+                                    }
+                                    else{
+                                        //delete prova->board[pos2+8];
+                                        prova->board[pos2+8]=nullptr;
+                                    }
+                                    prova->board[pos2]=prova->board[pos1];
+                                    prova->board[pos1]=nullptr;
+                                    prova->board[pos2]->setPosizione(pos2);
+                                    if (prova->W(board[pos1]->getColore()))
+                                        throw Mossa_illegale();
+                                    if(board[pos1]->getColore()){
+                                        delete board[pos2-8];
+                                        board[pos2-8]=nullptr;
+                                    }
+                                    else{
+                                        delete board[pos2+8];
+                                        board[pos2+8]=nullptr;
+                                    }
+                                    board[pos2]=board[pos1];
+                                   board[pos1]=nullptr;
+                                   board[pos2]->setPosizione(pos2);
+                                   return;
+                               }
+                            }
+                        }
+                    Scacchiera* prova=new Scacchiera(*this);
+                    if(prova->board[pos2])
+                     delete prova->board[pos2];
+                      prova->board[pos2]=prova->board[pos1];
+                      prova->board[pos1]=nullptr;
+                      prova->board[pos2]->setPosizione(pos2);
+                      if (prova->W(board[pos1]->getColore()))
+                          throw Mossa_illegale();
 
+                    if(board[pos2])
+                        delete board[pos2];
+                    board[pos2]=board[pos1];
+                    board[pos1]=nullptr;
+                    board[pos2]->setPosizione(pos2);
+                    return;
+                    }
+               }
+            }
+}
+        throw Mossa_Imposs();
+}
 
